7456cdf5d94af62d6fcb4bd97855f2ea562a946c
beefchief <c.lang.1993@gmail.com>
2024-10-07 20:23:28 -0500

    homework complete

--- server.js
+++ server.js
@@ -2,4 +2,32 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Employees!");
+});
+
+const employees = require("./employees");
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+app.get("/employees/random", (req, res) => {
+  const i = Math.floor(Math.random() * employees.length);
+  res.json(employees[i]);
+});
+
+app.get("/employees/:id", (req, res) => {
+  const { id } = req.params;
+  if (id < 0 || id >= employees.length) {
+    res.status(404).send("Employee does not exist in system");
+  } else {
+    res.json(employees[id]);
+  }
+});
+
+app.listen(PORT, () => {
+  `listening on port ${PORT}`;
+});
